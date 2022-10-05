8701df673146d1c940e728bbdcd2dcf3ffe38087
Kenanimok <touch@sutha-MacBook-Pro.local>
2022-10-05 15:06:19 +0700

    fix useformik

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import ChartUi from "./pages/chartui/chartUi";
 import Allchart from "./pages/allChart/all";
 import Report from "./pages/reportdashboard/report";
 import Froala_t from "./pages/froala/froala";
-import Crud_insert from "./pages/form/insert_f";
+import Crud_insert from "./pages/form_basic_reset/insert_f";
+import Basic_Useformik from "./pages/form_basic_reset/formik_basic";
 function App() {
   return (
     <div>
@@ -19,7 +20,8 @@ function App() {
             <Route path="/all" element={<Allchart />}></Route>
             <Route path="/report" element={<Report />} />
             <Route path="/form" element={<Crud_insert />} />
-            <Route path="/froala" element={<Froala_t />} />
+            <Route path="/useformik" element={<Basic_Useformik />} />
+            {/* <Route path="/froala" element={<Froala_t />} /> */}
           </Routes>
         </Corelayout>
       </BrowserRouter>
---
+++ src/pages/form_basic_reset/formik_basic.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useFormik } from "formik";
+
+const Basic_Useformik = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+      <button type="reset">reset</button>
+    </form>
+  );
+};
+export default Basic_Useformik;
